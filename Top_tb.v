ae7f84b630b680e72f6458e4ac6ab398c5c6a61c
rfuentesgtz <70235891+rfuentesgtz@users.noreply.github.com>
2021-09-03 09:05:00 -0700

    Add files via upload

--- Top_tb.v
+++ Top_tb.v
@@ -1,30 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/02/2021 11:28:49 AM
+// Design Name: 
+// Module Name: Top_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module Top_tb( );
 
   reg Clk, Rst;
   wire [31:0]Instruction;
   wire [6:0] out7;
-  wire reg[7:0] en_out; 
+  wire [7:0] en_out; 
   
   Top Top_1(Rst, Clk, Instruction, out7, en_out);
   
-  always @ begin //period is 20 ns! that is why we increment every 10 ns.
+  always begin //period is 10 ns! that is why we increment every 5 ns.
     Clk <= 0;
-    #10;
+    #5;
     Clk <= 1;
-    #10;
+    #5;
   end
   
   initial begin
     Rst <= 1;
     //move 2 clk cycles ahead
-    @(Posedge Clk);
-    @(Posedge Clk);
-    #5; // do this so RST and Clk do not change at same time. 
+    @(posedge Clk);
+    @(posedge Clk);
+    #2; // do this so RST and Clk do not change at same time. 
     Rst <= 0;
-    @(Posedge Clk);
-    #500;
-    #5;
-    Rst <= 1;
+    @(posedge Clk);
+    //#500;
+    //#2;
+    //Rst <= 1;
   end
 endmodule;
